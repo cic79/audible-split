383b982e3924f079f93efc648183f4fb6abb99ae
Federico Conte <federico.conte@quentral.com>
2022-01-05 15:30:02 +0100

    first try

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
 
-# Press Alt+Shift+X to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime
+import json
+import os
+import subprocess
+
+import srt
+from vosk import Model, KaldiRecognizer, SetLogLevel
+
+SetLogLevel(-1)
+
+AUDIO_FILE = '/home/fconte/Downloads/audible/James S.A. Corey - The Expanse 2 - Caliban. La guerra.mp3'
+MODEL = 'models/vosk-model-small-it'
+SAMPLE_RATE = 16000
+WORDS_PER_LINE = 7
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+Shift+B to toggle the breakpoint.
+def transcribe():
+    results = []
+    subs = []
+    model = Model(MODEL)
+    rec = KaldiRecognizer(model, SAMPLE_RATE)
+    rec.SetWords(True)
+    while True:
+        data = process.stdout.read(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            results.append(rec.Result())
+    results.append(rec.FinalResult())
+
+    for i, res in enumerate(results):
+        jres = json.loads(res)
+        if 'result' not in jres:
+            continue
+        words = jres['result']
+        for j in range(0, len(words), WORDS_PER_LINE):
+            line = words[j: j + WORDS_PER_LINE]
+            s = srt.Subtitle(index=len(subs),
+                             content=' '.join([l['word'] for l in line]),
+                             start=datetime.timedelta(seconds=line[0]['start']),
+                             end=datetime.timedelta(seconds=line[-1]['end']))
+            subs.append(s)
+    return subs
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if not os.path.exists(MODEL):
+    print('Please download the model from https://alphacephei.com/vosk/models and unpack as in the MODEL folder.')
+    exit(1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+wav_file = '%s.wav' % AUDIO_FILE.rsplit('.', 1)[0]
+result = subprocess.run(['ffmpeg', '-i', AUDIO_FILE, '-ar', str(SAMPLE_RATE), '-ac', '1', wav_file])
+if result.returncode is not 0:
+    print(result)
+    exit(result.returncode)
+print('WAV READY...')
+
+process = subprocess.Popen(
+    ['ffmpeg', '-loglevel', 'quiet', '-i', wav_file, '-ar', str(SAMPLE_RATE), '-ac', '1', '-f', 's16le', '-'],
+    stdout=subprocess.PIPE)
+print('WAV OPENED...')
+
+print(srt.compose(transcribe()))
+
+print('COMPLETED!')
