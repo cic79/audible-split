c97f0f61c6b3a089658963c07fa9afdfd16c627b
Federico Conte <federico.conte@quentral.com>
2022-01-09 17:19:58 +0100

    added xml podcast generator

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from utils import exec_cmd, is_tool, query_yes_no, get_secs, get_elapsed_secs, n
 
 AUDIO_FILE = '/home/fconte/Downloads/audible/James S.A. Corey - The Expanse 2 - Caliban. La guerra.mp3'
 base_name_with_path = AUDIO_FILE.rsplit('.', 1)[0]
-base_name = os.path.basename(base_name_with_path)
+PERFORMER, TITLE = os.path.basename(base_name_with_path).split(' - ', 1)
 WAV_FILE = f'{base_name_with_path}.wav'
 OUTPUT_FILE = f'{base_name_with_path}.output'
 SRT_FILE = f'{base_name_with_path}.srt'
@@ -95,7 +95,7 @@ else:
 
 # Generate output file with vosk
 if not output_file_exists or overwrite_output_file:
-    sys.stdout.write(f'\nReading \'{WAV_FILE}\' file...')
+    sys.stdout.write(f'\nReading \'{WAV_FILE}\' file...\n')
     with open(OUTPUT_FILE, 'w') as output:
         SetLogLevel(-1)
         model = Model(MODEL)
@@ -116,7 +116,7 @@ else:
 
 # Convert output vosk in srt
 if not srt_file_exists or overwrite_srt_file:
-    sys.stdout.write(f'\nReading \'{OUTPUT_FILE}\' file...\n')
+    sys.stdout.write(f'\nReading \'{OUTPUT_FILE}\' file...')
     with open(OUTPUT_FILE, 'r') as f:
         results = f.readlines()
     subs = []
@@ -171,12 +171,11 @@ with open(SRT_FILE, 'r') as f:
             break
 
 # Generate cue file
-PERFORMER = 'Audible'
 if not cue_file_exists or overwrite_cue_file:
     with open(CUE_FILE, 'w') as f:
         track_number = 1
         f.write(f'PERFORMER "{PERFORMER}"\r\n')
-        f.write(f'TITLE "{base_name}"\r\n')
+        f.write(f'TITLE "{TITLE}"\r\n')
         f.write(f'FILE "{AUDIO_FILE}" MP3\r\n')
         first_chapter_time = chapters[0][0]
         if get_secs(first_chapter_time) > 6:
---
+++ podcast.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from bs4 import BeautifulSoup
+
+# Url condiviso contenente i podcast:
+# https://www.dropbox.com/sh/2zbtmzxkk44qmgk/AACfbAgwyD0A9TAB3EdANO9ja?dl=0
+CONTENT = '''
+<html>
+<table role="table" class="mc-table sl-list-container"></table>
+</html>
+'''
+title = ''
+podcasts = []
+xml_name = xml_link = img_name = img_link = None
+soup = BeautifulSoup(CONTENT, 'html.parser')
+results = soup.find(class_='mc-table').find_all('a', class_='')
+for result in results:
+    link = result.attrs['href'].split('?')[0] + '?dl=1'
+    filename = result.attrs['aria-label']
+    name, extension = filename.rsplit('.', 1)
+    if extension == 'xml':
+        xml_name = filename
+        xml_link = link
+    elif extension in ('png', 'jpg'):
+        performer, title = name.split(' - ', 1)
+        img_name = filename
+        img_link = link
+    else:
+        podcasts.append({'name': filename, 'link': link})
+
+if not(xml_name and xml_link and img_name and img_link):
+    sys.stderr.write('ERROR: Houston we have a problem! At least one file not found (xml or image).')
+    exit(1)
+
+audible_start = f'''<?xml version="1.0" encoding="UTF-8"?>
+<rss xmlns:itunes="https://www.itunes.com/dtds/podcast-1.0.dtd"
+     xmlns:googleplay="https://www.google.com/schemas/play-podcasts/1.0" xmlns:atom="http://www.w3.org/2005/Atom"
+     version="2.0" xml:lang="it-IT">
+    <channel>
+        <atom:link href="{xml_link}" rel="self" type="application/rss+xml"></atom:link>
+        <title><![CDATA[{title}]]></title>
+        <itunes:author>Audible</itunes:author>
+        <language>it-it</language>
+        <itunes:name>{title}</itunes:name>
+        <itunes:image href="{img_link}"></itunes:image>
+        <googleplay:image href="{img_link}"></googleplay:image>
+'''
+audible_item = f'''
+        <item>
+            <title><![CDATA[%(podcast_name)s]]></title>
+            <link>%(podcast_link)s</link>
+            <description></description>
+            <itunes:author></itunes:author>
+            <itunes:image href="{img_link}"></itunes:image>
+            <googleplay:image href="{img_link}"></googleplay:image>
+            <enclosure length="10000" type="audio/mpeg" url="%(podcast_link)s"></enclosure>
+            <pubDate>Thu, 28 Dec 2021 18:55:26 +0100</pubDate>
+            <guid>%(podcast_link)s</guid>
+        </item>
+'''
+audible_end = '''
+    </channel>
+</rss>'''
+audible = audible_start
+for podcast in podcasts:
+    audible += audible_item % {'podcast_name': podcast['name'], 'podcast_link': podcast['link']}
+audible += audible_end
+with open('podcast.xml', 'w') as f:
+    f.write(audible)
+
+sys.stdout.write(f'\npodcast.xml available at:\n{xml_link}')
